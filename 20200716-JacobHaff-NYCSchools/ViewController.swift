78e59f4b4f4edc6b57a66ede8a0b902634c0baf6
Jacob Haff <jacobhaff2@gmail.com>
2020-07-16 17:23:41 -0400

    Initial Commit

---
+++ 20200716-JacobHaff-NYCSchools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20200716-JacobHaff-NYCSchools
+//
+//  Created by Jacob Haff on 7/16/20.
+//  Copyright © 2020 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
